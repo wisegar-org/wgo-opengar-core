8914ec1751471c5d4628705337f981fd01a64336
Liana <lianabeatriz93@gmail.com>
2021-02-25 09:17:46 -0500

    Test commit

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,19 @@ export * from "./interfaces/IApiResponse";
 export * from "./interfaces/IJwtSettings";
 export * from "./interfaces/IUser";
 export * from "./interfaces/IUserRole";
+
 /**
  * @class Models Exports
  */
 export * from "./models/TokenResult";
+
 /**
  * @class Services Exports
  */
 export * from "./services/ApiRestService";
 export * from "./services/CacheService";
 export * from "./services/JwtService";
+
 /**
  * @class Utilities Exports
  */
