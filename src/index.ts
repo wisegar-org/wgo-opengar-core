fcf47baa5eb9eedd4e394a9eed2eaff13747e9ee
Liana <lianabeatriz93@gmail.com>
2021-04-06 14:40:21 -0400

    Add index export functions, instances

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,14 @@
 /**
- *  @interface Exports Database Entities
+ * @module Client
  */
-export * from "./server/database/entities/RolEntity";
-export * from "./server/database/entities/Session";
-export * from "./server/database/entities/UserEntity";
-export * from "./server/database/entities/MediaEntity";
-export * from "./server/database/entities/OGBaseEntity";
+export * from "./client";
 
 /**
- * @enum Database Enums
+ * @module Server
  */
-export * from "./shared/models/enums/MediaEntityTypeEnum";
-export * from "./shared/models/enums/RolEntityEnum";
+export * from "./server";
 
 /**
- *  @interface Exports
+ * @module Shared
  */
-export * from "./shared/interfaces/IApiResponse";
-export * from "./shared/interfaces/IJwtSettings";
-export * from "./shared/interfaces/IUser";
-export * from "./shared/interfaces/IUserRole";
-
-/**
- * @class Models Exports
- */
-export * from "./shared/models/TokenResult";
-export * from "./shared/models/enums/MediaEntityTypeEnum";
-export * from "./shared/models/enums/RolEntityEnum";
-
-/**
- * @class Services Exports
- */
-export * from "./client/services/ApiRestService";
-export * from "./client/services/CacheService";
-export * from "./server/services/JwtService";
-export * from "./server/services/EmailService";
-export * from "./shared/services/UserDataService";
-
-/**
- * @class Utilities Exports
- */
-export * from "./shared/utils/ObjExt";
+export * from "./shared";
---
+++ src/server/index.ts
@@ -0,0 +1,15 @@
+/**
+ *  @interface Exports Database Entities
+ */
+export * from "./database/entities/RolEntity";
+export * from "./database/entities/Session";
+export * from "./database/entities/UserEntity";
+export * from "./database/entities/MediaEntity";
+export * from "./database/entities/OGBaseEntity";
+
+
+/**
+ * @class Services Exports
+ */
+export * from "./services/JwtService";
+export * from "./services/EmailService";
