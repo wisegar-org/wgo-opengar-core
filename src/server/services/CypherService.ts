6a5c03b1074b772b0dc9ec2b867bbedb1c7363f1
yariel <info@wisegar.org>
2021-05-05 12:45:01 +0200

    Add decypherData

--- src/server/services/CypherService.ts
+++ src/server/services/CypherService.ts
@@ -1,11 +1,12 @@
 import { GetCypherKey } from './EnvService';
-import { createCipheriv, randomBytes } from 'crypto';
+import { createCipheriv, createDecipheriv, randomBytes } from 'crypto';
 
 const algorithm = 'aes256';
 const bufferEncoding = 'latin1';
 const randomBytesSize = 16;
 const outputEncoding = 'hex';
 const inputEncoding = 'utf8';
+const initVetorDataSeparator = ':';
 
 /**
  * Data cypher
@@ -20,4 +21,46 @@ export const cypherData = (data: any): string => {
   const encrypted = encrypt.update(JSON.stringify(data), inputEncoding, outputEncoding);
   const encryptedFinal = encrypted + encrypt.final(outputEncoding);
   return initVector.toString(outputEncoding) + ':' + encryptedFinal;
+};
+
+/**
+ * Data decypher
+ * @param cypherdata Most a already cyphered data
+ * @returns json data object
+ */
+export const decypherData = (cypherdata: string): string => {
+  const cypherKey = GetCypherKey();
+  const keyBuffer = Buffer.from(cypherKey, bufferEncoding);
+  const initVector = getCypherDataInitVector(cypherdata);
+  const cypgherContent = getCypherData(cypherdata);
+  const decipher = createDecipheriv(algorithm, keyBuffer, initVector);
+  const deciphered = decipher.update(cypgherContent, outputEncoding, inputEncoding);
+  const decipheredFinal = deciphered + decipher.final(inputEncoding);
+  return JSON.parse(decipheredFinal);
+};
+
+export const getCypherDataInitVector = (cypherdata: string) => {
+  if (!cypherdata) throw 'GetCypherDataInitVector: invalid cypherdata parameter!';
+
+  if (!cypherdata.includes(initVetorDataSeparator))
+    throw 'GetCypherDataInitVector: invalid cypherdata separator parameter!';
+
+  const splittedCypherData = cypherdata.split(initVetorDataSeparator);
+  if (!splittedCypherData || splittedCypherData.length < 1)
+    throw 'GetCypherDataInitVector: impossible to split the cypherdata!';
+
+  return splittedCypherData[0];
+};
+
+export const getCypherData = (cypherdata: string) => {
+  if (!cypherdata) throw 'GetCypherDataInitVector: invalid cypherdata parameter!';
+
+  if (!cypherdata.includes(initVetorDataSeparator))
+    throw 'GetCypherDataInitVector: invalid cypherdata separator parameter!';
+
+  const splittedCypherData = cypherdata.split(initVetorDataSeparator);
+  if (!splittedCypherData || splittedCypherData.length < 1)
+    throw 'GetCypherDataInitVector: impossible to split the cypherdata!';
+
+  return splittedCypherData[1];
 };
