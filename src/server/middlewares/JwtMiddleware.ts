fa2ee579b4562bf5b78e7f3a423a41bfa28072eb
yarielre <yariel.re@gmail.com>
2021-05-11 21:01:39 +0200

    Update JwtMiddleware

--- src/server/middlewares/JwtMiddleware.ts
+++ src/server/middlewares/JwtMiddleware.ts
@@ -2,12 +2,16 @@ import express from 'express';
 import { IServerOptions } from '../models/ServerOptions';
 import { AccessTokenData, jwtMiddleware } from '../services/JwtAuthService';
 
-export const jwt = async (options: IServerOptions) => {
-  return async (req: express.Request, res: express.Response, next: () => void) => {
+export const jwt = (options: IServerOptions) => {
+  return (req: express.Request, res: express.Response, next: () => void) => {
     const tokenData: AccessTokenData = jwtMiddleware(req, res);
     if (tokenData) {
-      (req as any).context = await options.context(tokenData);
+      options.context(tokenData).then((result) => {
+        (req as any).context = result;
+        next();
+      });
+    } else {
+      next();
     }
-    next();
   };
 };
