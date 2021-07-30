2b06911a4a352af6f31877da3a27db303a807b52
yariel <info@wisegar.org>
2021-07-30 11:11:08 +0200

    Add validator utils funtions

---
+++ src/utils/Validator.ts
@@ -0,0 +1,32 @@
+import _ from 'lodash';
+import * as dns from 'dns';
+
+export const isNullOrUndefined = (obj: any) => {
+  return _.isNull(obj) || _.isUndefined(obj);
+};
+
+/**
+ * @function isEmail Permette la validazione di una striga che rappresenta un indirizzo email
+ * Possibile anche utilizzare i validatori della libreria class-validator (@IsEmail())
+ */
+export const isEmail = (email: string): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    if (_.isUndefined(email) || _.isNull(email) || _.isEmpty(email)) {
+      reject('The email is required');
+    }
+    const predomain = email.split('@')[0];
+    if (_.isUndefined(predomain) || _.isNull(predomain) || _.isEmpty(predomain)) {
+      reject('The email is required');
+    }
+
+    let mxDomain = email.split('@')[1];
+    if (_.isUndefined(mxDomain) || _.isNull(mxDomain) || _.isEmpty(mxDomain)) {
+      reject('The domain is required');
+    }
+
+    dns.resolveMx(mxDomain, async (err, adresses) => {
+      if (err) reject(err);
+      resolve();
+    });
+  });
+};
