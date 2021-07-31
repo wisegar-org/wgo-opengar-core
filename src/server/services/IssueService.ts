88dd19d70e51d70ebe6c2d78ca202b6ddcc5d77f
yariel <info@wisegar.org>
2021-07-31 16:31:33 +0200

    Add TODO comment on IssueService

--- src/server/services/IssueService.ts
+++ src/server/services/IssueService.ts
@@ -3,28 +3,24 @@ import { Octokit } from '@octokit/core';
 import { IssueForm } from '../models/IssueForm';
 
 export default class IssueService {
-  
   /**
    *
    */
-  constructor() {
-    
-  }
+  constructor() {}
 
   async sendIssue(issue: IssueForm) {
     const config = GetGenericConfig();
     const octokit = new Octokit({ auth: config.GITHUB_TOKEN });
 
     const response = await octokit.request('POST /repos/{owner}/{repo}/issues', {
-        owner: issue.owner,
-        repo: issue.repo,
-        title: issue.title,
-        body: issue.body
-      });
-
-      return {
-        succeeded: true,
-      };
+      owner: issue.owner,
+      repo: issue.repo,
+      title: issue.title,
+      body: issue.body,
+    });
+    // TODO: Controlar que sea realmente succeded. Utilizando la response?
+    return {
+      succeeded: true,
+    };
   }
-  
 }
