c0f5fc30be43c90f211cb4d1fb1a4975788afbec
Liana Juliá <lianabeatriz93@gmail.com>
2021-08-27 13:54:30 -0400

    Fix rename Session entity name

--- src/server/database/entities/SessionEntity.ts
+++ src/server/database/entities/SessionEntity.ts
@@ -1,20 +1,20 @@
-import { Entity, Column } from "typeorm";
-import { ISessionLicenze } from "../../graphql/Models";
-import { OGBaseEntity } from "./OGBaseEntity";
+import { Entity, Column } from 'typeorm';
+import { ISessionLicenze } from '../../graphql/Models';
+import { OGBaseEntity } from './OGBaseEntity';
 
-@Entity()
-export class Session extends OGBaseEntity {
+@Entity({ name: 'session' })
+export class SessionEntity extends OGBaseEntity {
   @Column()
   userId: string;
   @Column()
   email: string;
-  @Column("text", { array: true, default: "{}" })
+  @Column('text', { array: true, default: '{}' })
   roles: string[];
-  @Column("json", { nullable: true })
+  @Column('json', { nullable: true })
   permissions: { [key: string]: string };
-  @Column({ type: "jsonb", nullable: true })
+  @Column({ type: 'jsonb', nullable: true })
   extra: { [key: string]: unknown };
-  @Column({ type: "jsonb", nullable: true })
+  @Column({ type: 'jsonb', nullable: true })
   applicazioni: ISessionLicenze;
   uuid: string;
 }
