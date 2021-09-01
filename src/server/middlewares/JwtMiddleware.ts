1ebf08357f6a4bbb18032c82b6f67532483c5d67
yarielre <yariel.re@gmail.com>
2021-09-01 16:08:45 +0200

    Update JwtMiddleware handler

--- src/server/middlewares/JwtMiddleware.ts
+++ src/server/middlewares/JwtMiddleware.ts
@@ -1,30 +1,49 @@
 import { AuthError } from '@wisegar-org/wgo-opengar-shared';
 import express from 'express';
+import { isNullOrUndefined } from '../../utils/Validator';
 import { IServerOptions } from '../models/ServerOptions';
 import { AccessTokenData, jwtMiddleware } from '../services/JwtAuthService';
+
+const isGraphql = (req: express.Request) => {
+  return req.originalUrl.includes('graphql');
+};
+
+const expressTokenErrorHandler = (res: express.Response, error: any) => {
+  console.error(error);
+  res.status(401);
+  res.statusMessage = `${AuthError.NotAuthorized}`;
+  res.end();
+};
+
+const graphqlTokenErrorHandler = (res: express.Response, error: any) => {
+  console.error(error);
+  res.status(200);
+  res.send(`{"errors":[{"message":"${AuthError.NotAuthorized}"}, {"message":"${error}"}], "data":null }`);
+  res.end();
+};
 
 export const jwt = (options: IServerOptions) => {
   return (req: express.Request, res: express.Response, next: () => void) => {
     try {
-      const tokenData: AccessTokenData = jwtMiddleware(req, res);
-      if (tokenData) {
-        options.context(tokenData).then((result) => {
-          (req as any).context = result;
-          next();
-        });
-      } else {
+      if (!isGraphql(req)) {
         next();
+        return;
       }
+      const tokenData: AccessTokenData = jwtMiddleware(req, res);
+      if (isNullOrUndefined(tokenData)) {
+        next();
+        return;
+      }
+      options.context(tokenData).then((result) => {
+        (req as any).context = result;
+        next();
+      });
     } catch (error) {
-      console.error(error);
-      if (req.originalUrl.includes('graphql')) {
-        res.status(200);
-        res.send(`{"errors":[{"message":"${AuthError.NotAuthorized}"}, {"message":"${error}"}], "data":null }`);
-      } else {
-        res.status(401);
-        res.statusMessage = `${AuthError.NotAuthorized}`;
+      if (isGraphql(req)) {
+        graphqlTokenErrorHandler(res, error);
+        return;
       }
-      res.end();
+      expressTokenErrorHandler(res, error);
     }
   };
 };
