ba6d7a5cebb7e90fb35f22b3573bb98e973c28ba
yarielre <yariel.re@gmail.com>
2021-09-24 18:16:17 +0200

    Update SixBankService

--- src/server/services/SixBankService/SixBankService.ts
+++ src/server/services/SixBankService/SixBankService.ts
@@ -6,11 +6,11 @@ export const getSixBanks = async (): Promise<ISixBankModel[]> => {
   try {
     const response = await axios.get(`${apiUrl}`);
     const jsonData = response.data;
-    if (!jsonData) throw `Six Banks Service - getSixBanks`;
-    const jkeys = Object.keys(jsonData);
+    if (!jsonData || !jsonData.entries) throw `Six Banks Service - getSixBanks data undefined`;
+    const jkeys = Object.keys(jsonData.entries);
     const banks: ISixBankModel[] = [];
     for (const key in jkeys) {
-      banks.push(jsonData[key]);
+      banks.push(jsonData.entries[key]);
     }
     return banks;
   } catch (error) {
